0810b6ddff0cfe3185f159f9bd45f0d2085b1bd0
zjsuperman1987 <250529492@qq.com>
2019-07-05 16:33:56 +0800

    'zj'

--- components/final-custom-view-pager/x-view-page.js
+++ components/final-custom-view-pager/x-view-page.js
@@ -15,7 +15,21 @@ Component({
    */
   data: {
     titles: ['首页', '热点', '热门推荐', '我爱我家', '我的资产'],
-    current: 0
+    colors: {
+      initStatusColor: '#202020',
+      firstStatusColor: '#FF4500',
+      secondStatusColor: '#FF7F50',
+      thirdStatusColor: '#FF6A6A',
+      endStatusColor: '#FF0000'
+    },
+    // firstStatusColor: '#F08080',
+    // secondStatusColor: '#FF6347',
+    // thirdStatusColor: '#FF4500',
+    // endStatusColor: '#FF0000'
+    current: 0,
+    initLocked: false,
+    middleLocked: false,
+    middlePosition: false, // 记录是否划过中间位置
   },
 
   /**
@@ -31,56 +45,168 @@ Component({
     },
     // 头部点击
     titleTap(e) {
-      console.log(e)
+      let current = e.currentTarget.dataset.index;
       this.setData({
-        current: e.target.dataset.index
+        current: current,
+        titleTouch: true
       })
     },
 
-    // 内容 swiper
-    _onSwiperChange(e) {
-      let current = e.detail.source === 'touch' ? e.detail.current : this.data.current
+    // 过度中
+    _onTransition(e) {
+      if (!this.data.titleTouch) {
+        let dx = e.detail.dx;
+        // 设置颜色
+        this._setTitleColor(dx);
+        // 设置 底边线 bottomLine
+        this._setBottomLine(e, dx);
+      }
+    },
+    // 结束
+    _onAnimationFinish(e) {
+      let bottomLineTranslateX = this.data.translateArr[e.detail.current],
+        bottomLineWidth = this.data.textWidth[e.detail.current].width,
+        titleColor = 'titleColor[' + e.detail.current + '].color';
+      // 释放锁
+      this.data.initLocked = false;
+      this.data.middleLocked = false;
+      this.data.middlePosition = false;
+      this.data.animation.width(bottomLineWidth);
+      this.data.animation.translate3d(bottomLineTranslateX).step();
+
       this.setData({
-        current: current
+        animationData: this.data.animation.export(),
+        current: e.detail.current,
+        titleTouch: false
       })
+
+      // 取消全命中
+      for (let i = 0; i < this.data.titles.length; i++) {
+        let color = 'titleColor[' + i + '].color';
+        this.setData({
+          [color]: this.data.colors.initStatusColor
+        })
+      }
+      this.setData({
+        [titleColor]: this.data.colors.endStatusColor
+      })
+    },
+    // 设置颜色
+    _setTitleColor(delta) {
+      let currentIndex, targetIndex;
+
+      currentIndex = this.data.current;
+
+      if (delta > 0) {
+        targetIndex = currentIndex === this.data.titles.length ? currentIndex : currentIndex + 1;
+      } else {
+        targetIndex = currentIndex === 0 ? 0 : currentIndex - 1;
+      }
+      // 开始位移
+      let currentColor = 'titleColor[' + currentIndex + '].color'
+      let targetColor = 'titleColor[' + targetIndex + '].color'
+
+      if (currentIndex !== targetIndex && !this.data.initLocked) {
+        this.setData({
+          [currentColor]: this.data.colors.firstStatusColor,
+          [targetColor]: this.data.colors.secondStatusColor
+        })
+        this.data.initLocked = true;
+      }
+      // 位移到中间
+      if (Math.abs(delta) > 375 / 2 && !this.data.middleLocked) {
+        this.setData({
+          [currentColor]: this.data.colors.thirdStatusColor,
+          [targetColor]: this.data.colors.thirdStatusColor,
+          middlePosition: true
+        })
+        this.data.middleLocked = true;
+      } else {
+        // 划过· 回退回来
+        if (this.data.middlePosition && Math.abs(delta) < 375 / 2) {
+          this.data.middleLocked = false;
+          this.setData({
+            [currentColor]: this.data.colors.firstStatusColor,
+            [targetColor]: this.data.colors.secondStatusColor,
+          })
+        }
+      }
+    },
+    // 设置底边线
+    _setBottomLine(_e, delta) {
+      let currentIndex, targetIndex;
+      currentIndex = this.data.current;
+
+      if (delta > 0) {
+        targetIndex = currentIndex === this.data.titles.length ? currentIndex : currentIndex + 1;
+      } else {
+        targetIndex = currentIndex === 0 ? 0 : currentIndex - 1;
+      }
+      // 需要移动距离
+      if (currentIndex !== targetIndex) {
+        let translateArr = this.data.translateArr
+        let distanceX = Math.abs(translateArr[targetIndex] - translateArr[currentIndex]);
+        let titleX = (distanceX / 375) * delta;
+        let title_l = 0; 
+        if (delta > 0) {
+          title_l = translateArr[currentIndex] + titleX;
+        }else {
+          title_l = translateArr[currentIndex] - titleX;
+        }
+        console.log(title_l, '..................')
+        this.data.animation.translate3d(title_l, 0, 0).step();
+        this.setData({
+          animationData: this.data.animation.export()
+        })
+      }
     }
   },
+
   ready() {
     let that = this,
-        query = wx.createSelectorQuery().in(this);
-// 头部计算 ·················
+      query = wx.createSelectorQuery().in(this);
     query.selectAll('.header-item').fields({
       size: true
     }, res => {
       that.setData({
         titleWidth: res
       })
-    }).exec(function () {
-      // 计算头部宽度
-      let headerWidth = 0;
-      for (let i = 0; i < that.data.titleWidth.length; i++) {
-        headerWidth += that.data.titleWidth[i].width;
-      }
-      that.setData({
-        headerWidth: headerWidth
-      })
-
+    }).exec(function() {
       query.selectAll('.header-font').fields({
         size: true
       }, res => {
         that.setData({
           textWidth: res,
-          bottomLineWidth: res[0].width
         })
-      }).exec(function () {
-        let titleWidth = that.data.titleWidth[0].width;
-        let textWidth = that.data.textWidth[0].width;
-        that.setData({
-          bottomLineTranslateX: (titleWidth - textWidth) / 2
-        })
-
+      }).exec(function() {
         // 计算位移位置
+        let translateArr = [];
+        let titleWidth = that.data.titleWidth;
+        let textWidth = that.data.textWidth;
+        for (let i = 0; i < titleWidth.length; i++) {
+          if (i === 0) {
+            let translateX = (titleWidth[0].width - textWidth[0].width) / 2;
+            translateArr.push(translateX);
+          } else {
+            // 第二个开始等于第一个 位移 加上 1， 2 单边距离
+            let translateX = translateArr[i - 1] + ((titleWidth[i - 1].width - textWidth[i - 1].width) / 2) + (titleWidth[i].width - textWidth[i].width) / 2 + textWidth[i - 1].width;
+            translateArr.push(translateX);
+          }
+        }
+        let animation = wx.createAnimation({
+          duration: 0,
+          timingFunction: 'linear'
+        })
 
+        animation.width(that.data.textWidth[0].width);
+        animation.translate3d(translateArr[0], 0, 0).step()
+
+        that.setData({
+          animation: animation,
+          translateArr: translateArr,
+          animationData: animation.export(),
+          'titleColor[0].color': that.data.colors.endStatusColor
+        })
       });
     });
   }
